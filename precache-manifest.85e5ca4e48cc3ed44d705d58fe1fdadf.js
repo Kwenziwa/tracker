709ce614001bd6d56c408bf0fbde158e3157f6e4
Kwenziwa <Kwenziwa@live.com>
2024-03-27 20:10:31 -0400

    Updates

---
+++ precache-manifest.85e5ca4e48cc3ed44d705d58fe1fdadf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6547d7cf214b0330515960bc200bbd3",
+    "url": "/tracker/index.html"
+  },
+  {
+    "revision": "a561377acf0ebcbc53b8",
+    "url": "/tracker/static/css/main.f7455dbd.chunk.css"
+  },
+  {
+    "revision": "7a29eb9db402bccdffbb",
+    "url": "/tracker/static/js/2.1261af49.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tracker/static/js/2.1261af49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a561377acf0ebcbc53b8",
+    "url": "/tracker/static/js/main.41d80f1e.chunk.js"
+  },
+  {
+    "revision": "7e2e9fbd28dbb8e2002d",
+    "url": "/tracker/static/js/runtime-main.ed40d8c8.js"
+  }
+]);
